792b5a3f4ca5bb9b7f56f6add105b87d8813977b
trippedBit <130838437+trippedBit@users.noreply.github.com>
2023-09-25 17:12:59 +0200

    feat: disabled debug messages

--- scripts/mission005.lua
+++ scripts/mission005.lua
@@ -13,8 +13,8 @@ NScript.Register(
 			 function(V, Data)
 				if MissionLib.GetStarSystemId() == 8 then
 					--NGUI.ShowInfoText({
-						--Text="ID_MISSION_INIT",
-						--Replace={"ID_MISSION5_ID"}})
+			--Text="ID_MISSION_INIT",
+			--Replace={"ID_MISSION5_ID"}})
 					return {Ready=true}
 				end
 				return {Ready=false}
@@ -59,9 +59,9 @@ NScript.Register(
 			 nil,
 			 "Achieved",
 			 function(V, Data)
-				NGUI.ShowInfoText({
-					Text="ID_MISSION_ACHIEVED",
-					Replace={"ID_MISSION5_ID"}})
+				--NGUI.ShowInfoText({
+					--Text="ID_MISSION_ACHIEVED",
+					--Replace={"ID_MISSION5_ID"}})
 				V.Step=0
 				return {
 					Credits=V.Reward,
@@ -73,9 +73,9 @@ NScript.Register(
 			 nil,
 			 "Failed",
 			 function(V, Data)
-				NGUI.ShowInfoText({
-					Text="ID_MISSION_FAILED",
-					Replace={"ID_MISSION5_ID"}})
+				--NGUI.ShowInfoText({
+					--Text="ID_MISSION_FAILED",
+					--Replace={"ID_MISSION5_ID"}})
 				
 				V.Step=0
 				return {Timeout=0}
@@ -103,81 +103,81 @@ NScript.Register(
 			 function(V, Data)
 				if Data.Enter == false then
 					--NGUI.ShowInfoText({
-						--Text="ID_MISSION_CREATE_WAYPOINTS",
-						--Replace={"ID_MISSION5_ID"}})
+			--Text="ID_MISSION_CREATE_WAYPOINTS",
+			--Replace={"ID_MISSION5_ID"}})
 					-- Create wing as time tracker
 					V.Wing1=NWing.Create({
-						WingType=WINGTYPE_FREIGHTER600CAP,
-						Number=1,
-						LevelOffset=0,
-						Race=RACE_RANDOM,
-						PosType=POSDESCR_EXPLICIT,
-						Position={-5000,-4500,-7000}}).Wing
+			WingType=WINGTYPE_FREIGHTER600CAP,
+			Number=1,
+			LevelOffset=0,
+			Race=RACE_RANDOM,
+			PosType=POSDESCR_EXPLICIT,
+			Position={-5000,-4500,-7000}}).Wing
 					V.Wing2=NWing.Create({
-						WingType=WINGTYPE_FREIGHTER600CAP,
-						Number=1,
-						LevelOffset=0,
-						Race=RACE_RANDOM,
-						PosType=POSDESCR_EXPLICIT,
-						Position={-8000,-4500,-10200}}).Wing
+			WingType=WINGTYPE_FREIGHTER600CAP,
+			Number=1,
+			LevelOffset=0,
+			Race=RACE_RANDOM,
+			PosType=POSDESCR_EXPLICIT,
+			Position={-8000,-4500,-10200}}).Wing
 					V.Wing3=NWing.Create({
-						WingType=WINGTYPE_FREIGHTER600CAP,
-						Number=1,
-						LevelOffset=0,
-						Race=RACE_RANDOM,
-						PosType=POSDESCR_EXPLICIT,
-						Position={-6500,-6500,-25000}}).Wing
+			WingType=WINGTYPE_FREIGHTER600CAP,
+			Number=1,
+			LevelOffset=0,
+			Race=RACE_RANDOM,
+			PosType=POSDESCR_EXPLICIT,
+			Position={-6500,-6500,-25000}}).Wing
 			
 					MissionLib.SetWingName(
-						V.Wing1,
-						"ID_MISSION005_CLIENT")
+			V.Wing1,
+			"ID_MISSION005_CLIENT")
 					MissionLib.SetWingName(
-						V.Wing2,
-						"ID_MISSION005_CLIENT")
+			V.Wing2,
+			"ID_MISSION005_CLIENT")
 					MissionLib.SetWingName(
-						V.Wing3,
-						"ID_MISSION005_CLIENT")
+			V.Wing3,
+			"ID_MISSION005_CLIENT")
 			
 					NWing.AddAction({
-						Wing=V.Wing1,
-						List=LIST_ACTIVE,
-						First=1,
-						Actions={{Type=AI_ACTION_WARTEN}}})
+			Wing=V.Wing1,
+			List=LIST_ACTIVE,
+			First=1,
+			Actions={{Type=AI_ACTION_WARTEN}}})
 					NWing.AddAction({
-						Wing=V.Wing2,
-						List=LIST_ACTIVE,
-						First=1,
-						Actions={{Type=AI_ACTION_WARTEN}}})
+			Wing=V.Wing2,
+			List=LIST_ACTIVE,
+			First=1,
+			Actions={{Type=AI_ACTION_WARTEN}}})
 					NWing.AddAction({
-						Wing=V.Wing3,
-						List=LIST_ACTIVE,
-						First=1,
-						Actions={{Type=AI_ACTION_WARTEN}}})
+			Wing=V.Wing3,
+			List=LIST_ACTIVE,
+			First=1,
+			Actions={{Type=AI_ACTION_WARTEN}}})
 					
 					-- Create rings
 					V.Ring1=NObject.CreateRing({
-						Position={-5000,-4500,-8000},
-						Front={90,0,0}}).Ring
+			Position={-5000,-4500,-8000},
+			Front={90,0,0}}).Ring
 					V.Ring2=NObject.CreateRing({
-						Position={-8000,-4500,-9500},
-						Front={90,0,0}}).Ring
+			Position={-8000,-4500,-9500},
+			Front={90,0,0}}).Ring
 					V.Ring3=NObject.CreateRing({
-						Position={-5000,-6500,-25000},
-						Front={90,0,0}}).Ring
+			Position={-5000,-6500,-25000},
+			Front={90,0,0}}).Ring
 			
 					-- Create waypoint in each ring
 					V.Waypoint1=NWaypoint.Create({
-						Position={-5000,-4500,-8000},
-						Radius=10,
-						Visible=true}).Waypoint
+			Position={-5000,-4500,-8000},
+			Radius=10,
+			Visible=true}).Waypoint
 					V.Waypoint2=NWaypoint.Create({
-						Position={-8000,-4500,-9500},
-						Radius=10,
-						Visible=true}).Waypoint
+			Position={-8000,-4500,-9500},
+			Radius=10,
+			Visible=true}).Waypoint
 					V.Waypoint3=NWaypoint.Create({
-						Position={-5000,-6500,-25000},
-						Radius=10,
-						Visible=true}).Waypoint
+			Position={-5000,-6500,-25000},
+			Radius=10,
+			Visible=true}).Waypoint
 					
 					-- Initialize variables for time measurement
 					V.StartTime=0
@@ -187,8 +187,8 @@ NScript.Register(
 					NPlayer.SelectTarget({Object=V.Waypoint1}) -- crashes if used with rings, works for waypoints and wings
 					
 					--NGUI.ShowInfoText({
-						--Text="ID_MISSION_CREATE_WAYPOINTS_DONE",
-						--Replace={"ID_MISSION5_ID"}})
+			--Text="ID_MISSION_CREATE_WAYPOINTS_DONE",
+			--Replace={"ID_MISSION5_ID"}})
 					V.Step=2
 				end
 			end
@@ -212,88 +212,88 @@ NScript.Register(
 			 function(V, Data)
 				if V.Step == 2 and Data.Leaving == true then
 					--NGUI.ShowInfoText({
-						--Text="ID_WAYPOINT_TRIGGERED"})
+			--Text="ID_WAYPOINT_TRIGGERED"})
 			
 					-- first get end time
 					-- start time has been initialized with 0 (zero) in the Station event
 					-- if the player triggers waypoint 2 or 3 before triggering waypoint 1, the needed time is the current time and therefore too high to win
 					if Data.Waypoint == V.Waypoint2 then
-						--NGUI.ShowInfoText({
-							--Text="ID_TIME_MEASUREMENT_END"})
-						V.EndTime=NGame.GetTime({}).Time
+			--NGUI.ShowInfoText({
+				--Text="ID_TIME_MEASUREMENT_END"})
+			V.EndTime=NGame.GetTime({}).Time
 			
-						V.TimeDifference=V.EndTime-V.StartTime
-						if V.TimeDifference < 30 then
-							--NGUI.ShowInfoText({
-								--Text="ID_TIME_BELOW_30"})
-							MissionLib.SetWingName(V.Wing2, "ID_TIME_BELOW_30")
-						else
-							--NGUI.ShowInfoText({
-								--Text="ID_TIME_ABOVE_30"})
-							MissionLib.SetWingName(V.Wing2, "ID_TIME_ABOVE_30")
-						end
+			V.TimeDifference=V.EndTime-V.StartTime
+			if V.TimeDifference < 30 then
+				--NGUI.ShowInfoText({
+					--Text="ID_TIME_BELOW_30"})
+				MissionLib.SetWingName(V.Wing2, "ID_TIME_BELOW_30")
+			else
+				--NGUI.ShowInfoText({
+					--Text="ID_TIME_ABOVE_30"})
+				MissionLib.SetWingName(V.Wing2, "ID_TIME_ABOVE_30")
+			end
 					elseif Data.Waypoint == V.Waypoint3 then
-						--NGUI.ShowInfoText({
-							--Text="ID_TIME_MEASUREMENT_END"})
-						V.EndTime=NGame.GetTime({}).Time
+			--NGUI.ShowInfoText({
+				--Text="ID_TIME_MEASUREMENT_END"})
+			V.EndTime=NGame.GetTime({}).Time
 			
-						V.TimeDifference=V.EndTime-V.StartTime
-						if V.TimeDifference < 160 then  -- first place
-							MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_160")
-							V.Reward=NPlayer.StandardReward({Factor=1}).Credits
-							-- drop artefact as additional reward
-							NContainer.Create({
-								Type=CONTAINER_POSITION,
-								Size=CONTAINER_ARTEFACT,
-								Position={-5000,-6500,-25000}
-							})
-						elseif V.TimeDifference < 165 then  -- second place
-							MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_165")
-							V.Reward=NPlayer.StandardReward({Factor=0.75}).Credits
-							-- drop artefact as additional reward
-							NContainer.Create({
-								Type=CONTAINER_POSITION,
-								Size=CONTAINER_ARTEFACT,
-								Position={-5000,-6500,-25000}
-							})
-						elseif V.TimeDifference < 170 then  -- third place
-							MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_170")
-							V.Reward=NPlayer.StandardReward({Factor=0.5}).Credits
-						else
-							MissionLib.SetWingName(V.Wing3, "ID_TIME_TOO_SLOW")
-							V.Reward=NPlayer.StandardReward({Factor=0.25}).Credits
-						end
+			V.TimeDifference=V.EndTime-V.StartTime
+			if V.TimeDifference < 160 then  -- first place
+				MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_160")
+				V.Reward=NPlayer.StandardReward({Factor=1}).Credits
+				-- drop artefact as additional reward
+				NContainer.Create({
+					Type=CONTAINER_POSITION,
+					Size=CONTAINER_ARTEFACT,
+					Position={-5000,-6500,-25000}
+				})
+			elseif V.TimeDifference < 165 then  -- second place
+				MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_165")
+				V.Reward=NPlayer.StandardReward({Factor=0.75}).Credits
+				-- drop artefact as additional reward
+				NContainer.Create({
+					Type=CONTAINER_POSITION,
+					Size=CONTAINER_ARTEFACT,
+					Position={-5000,-6500,-25000}
+				})
+			elseif V.TimeDifference < 170 then  -- third place
+				MissionLib.SetWingName(V.Wing3, "ID_TIME_BELOW_170")
+				V.Reward=NPlayer.StandardReward({Factor=0.5}).Credits
+			else
+				MissionLib.SetWingName(V.Wing3, "ID_TIME_TOO_SLOW")
+				V.Reward=NPlayer.StandardReward({Factor=0.25}).Credits
+			end
 			
-						-- Mission done, mark it as achieved
-						NWaypoint.Show({
-							Waypoint=V.Waypoint1,
-							Show=false
-						})
-						NWaypoint.Show({
-							Waypoint=V.Waypoint2,
-							Show=false
-						})
-						NWaypoint.Show({
-							Waypoint=V.Waypoint3,
-							Show=false
-						})
-						NMission.SetState({State=MSTATE_ACHIEVED})
+			-- Mission done, mark it as achieved
+			NWaypoint.Show({
+				Waypoint=V.Waypoint1,
+				Show=false
+			})
+			NWaypoint.Show({
+				Waypoint=V.Waypoint2,
+				Show=false
+			})
+			NWaypoint.Show({
+				Waypoint=V.Waypoint3,
+				Show=false
+			})
+			NMission.SetState({State=MSTATE_ACHIEVED})
 					end
 			
 					-- now get start time
 					if Data.Waypoint == V.Waypoint1 or Data.Waypoint == V.Waypoint2 then
-						NGUI.ShowInfoText({
-							Text="ID_TIME_MEASUREMENT_START"})
-						V.StartTime=NGame.GetTime({}).Time
+			--NGUI.ShowInfoText({
+				--Text="ID_TIME_MEASUREMENT_START"})
+			V.StartTime=NGame.GetTime({}).Time
 			
-						-- select next ring
-						if Data.Waypoint == V.Waypoint1 then
-							-- select waypoint 2 as target
-							NPlayer.SelectTarget({Object=V.Waypoint2}) -- crashes if used with rings, works for waypoints and wings
-						else
-							-- select waypoint 3 as target
-							NPlayer.SelectTarget({Object=V.Waypoint3}) -- crashes if used with rings, works for waypoints and wings
-						end
+			-- select next ring
+			if Data.Waypoint == V.Waypoint1 then
+				-- select waypoint 2 as target
+				NPlayer.SelectTarget({Object=V.Waypoint2}) -- crashes if used with rings, works for waypoints and wings
+			else
+				-- select waypoint 3 as target
+				NPlayer.SelectTarget({Object=V.Waypoint3}) -- crashes if used with rings, works for waypoints and wings
+			end
 					end
 				end
 			end
